28c664062e118c7fad5ca9b9120cee9c0162e818
DavidHuey1998 <huey1998@gwu.edu>
2019-04-22 21:01:50 -0400

    Fix crashes that resulted from username id

---
+++ app/src/main/java/edu/gwu/horizons/MainActivity.kt
@@ -0,0 +1,180 @@
+package edu.gwu.horizons
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+//import com.crashlytics.android.Crashlytics
+//import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+    private lateinit var signup: Button
+    private lateinit var remember: CheckBox
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    //private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+            val enableButton: Boolean = inputtedUsername.isNotEmpty() && inputtedPassword.isNotEmpty()
+
+            login.isEnabled = enableButton
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //val prefs = getSharedPreferences("horizons", Context.MODE_PRIVATE) //sharedPreferences so far
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        //firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        signup = findViewById(R.id.signup)
+
+        Log.d("MainActivity", "onCreate called")
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+
+        //if (prefs.getBoolean("REMEMBER", false)) {
+        username.addTextChangedListener(textWatcher)
+        password.addTextChangedListener(textWatcher)
+        /*} else {
+            username.setText(prefs.getString("SAVED_USERNAME", ""))
+            password.setText(prefs.getString("SAVED_PASSWORD", ""))
+        }
+
+        signup.setOnClickListener {
+
+            //Crashlytics.getInstance().crash()
+
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+
+            firebaseAuth.createUserWithEmailAndPassword(
+                inputtedUsername,
+                inputtedPassword
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val currentUser: FirebaseUser? = firebaseAuth.currentUser
+                    Toast.makeText(
+                        this,
+                        "Registered as: ${currentUser!!.email}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    val exception = task.exception
+                    Toast.makeText(
+                        this,
+                        "Failed to register: $exception",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+        login.setOnClickListener {
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+
+            firebaseAuth.signInWithEmailAndPassword(
+                inputtedUsername,
+                inputtedPassword
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //firebaseAnalytics.logEvent("login_success", null)
+
+                    val currentUser: FirebaseUser? = firebaseAuth.currentUser
+                    Toast.makeText(
+                        this,
+                        "Logged in as: ${currentUser!!.email}",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    // User logged in, advance to the next screen
+                    //val intent: Intent = Intent(this, ChoicesActivity::class.java)
+                    //startActivity(intent)
+
+                    remember.setOnCheckedChangeListener { _, isChecked ->   //sharedpreferences for the remember checkbox
+                        if (isChecked) {
+                            prefs.edit().putString("SAVED_USERNAME", inputtedUsername).apply()
+                            prefs.edit().putString("SAVED_PASSWORD", inputtedPassword).apply()
+                            prefs.edit().putBoolean("REMEMBER", true).apply()
+                        }
+
+                    }
+                } else {
+                    val exception = task.exception
+                    Toast.makeText(
+                        this,
+                        "Failed to login: $exception",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val reason: String = if (exception is FirebaseAuthInvalidCredentialsException) {
+                        "invalid_credentials"
+                    } else {
+                        "generic_failure"
+                    }
+
+                    val bundle = Bundle()
+                    bundle.putString("error_reason", reason)
+
+                    //firebaseAnalytics.logEvent("login_failed", bundle)
+                }
+            }
+
+
+        }*/
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy called")
+    }
+}
