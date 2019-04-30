9c6be4e0b93807d041d877192f26282eb87aa435
DavidHuey1998 <huey1998@gwu.edu>
2019-04-30 17:41:02 -0400

    fix buggy post request

--- app/src/main/java/edu/gwu/horizons/DiscogsManager.kt
+++ app/src/main/java/edu/gwu/horizons/DiscogsManager.kt
@@ -2,13 +2,15 @@ package edu.gwu.horizons
 
 import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+import java.sql.Timestamp
 import java.util.concurrent.TimeUnit
 
 class DiscogsManager {
 
     private val okHttpClient: OkHttpClient
 
-    private var oAuthToken: String? = null
+    private var oAuthToken: String? = "EqmUZjaWTXzZRPFmccEDEUvEVMsBxZcXACkfAazY"    //I'm just gonna hardcode it in
 
     init {
         val builder = OkHttpClient.Builder()
@@ -35,9 +37,25 @@ class DiscogsManager {
 
         val consumerKey =
                 "OMtKOzruKRvCvNlFEzqU"
-        val request: Request.Builder()  //post request for the discogs API
+        val consumerSecret =
+                "vsnWOJiSxeQxOGxEnWvCDejGRncLKVct"
+        val currentTime = Timestamp(System.currentTimeMillis())
+
+        //this is the post request with the following parameters:
+        /*OAuth oauth_consumer_key="your_consumer_key",
+            * oauth_nonce="random_string_or_timestamp",
+            * oauth_token="oauth_token_received_from_step_2"
+            * oauth_signature="your_consumer_secret&",
+            * oauth_signature_method="PLAINTEXT",
+            * oauth_timestamp="current_timestamp",
+            * oauth_verifier="users_verifier"
+        */
+
+        val request: Request = Request.Builder()
             .url("https://api.discogs.com/oauth/access_token")
-            .header("Authorization", "$consumerKey")
+            .header("oauth_consumer_key", consumerKey)
+            .header("oauth_token", oAuthToken)
+            .header("oauth_signature", consumerSecret)
             .post(
                 RequestBody.create(
                     MediaType.parse("application/x-www-form-urlencoded"),
@@ -45,6 +63,14 @@ class DiscogsManager {
                 )
             )
             .build()
+
+        okHttpClient.newCall(request).enqueue( object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                errorCallback(e)
+                
+            }
+
+        })
     }
 
 }
